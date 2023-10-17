eb39a46255b4bfb0e2bbc1eeee66ef6fb4824de2
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-10-16 23:42:14 -0300

    chore: login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      reset_session
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "olá amigo!"
+    else
+      redirect_to sessions_path, alert: "Credenciais erradas"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,19 @@ Rails.application.routes.draw do
   #
   root 'home#home'
 
+  # Login
+  resources :sessions, only: [:new, :create] do
+    collection do
+      delete "sign_out", to: "sessions#destroy", as: "sign_out"
+    end
+  end
+
   # Formulário de Login
-  get '/login', to: 'sessions#new', as: 'sessions'
+  #get '/login', to: 'sessions#new', as: 'sessions'
 
   # Rota para lidar com a submissão do formulário de sessions
-  post '/login', to: 'sessions#create'
+  #post '/login', to: 'sessions#create'
 
   # Rota para logout
-  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  #delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
