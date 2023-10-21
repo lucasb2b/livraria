b2bfe2e34d310586398d2db337cbff478ee64cdb
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-10-20 22:54:19 -0300

    chore: Add an author to the system

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,33 +1,34 @@
 class AuthorController < ApplicationController
-  before_action :set_author, only: [:show, :edit, :update, :destroy]
-
   def index
     @authors = Author.all
   end
-
   def show
     @author = Author.find(params[:id])
   end
-
   def new
     @author = Author.new
   end
+  def edit
+    @author = Author.find(params[:id])
+  end
+  def update
+    author = Author.find(params[:id])
+    author.update(author_params)
 
+    redirect_to dashboard_author_path
+  end
   def create
-    @author = Author.new(author_params)
-
-    if @author.save
-      redirect_to dashboard_author_path, notice: 'Autor salvo com sucesso.'
-    else
-      render dashboard_path
-    end
+    author = Author.new(author_params)
+    author.save
+    redirect_to dashboard_author_path
+  end
+  def destroy
+    author = Author.find(params[:id])
+    author.destroy
+    redirect_to dashboard_author_path
   end
 
   private
-
-  def set_author
-    author = Author.find(params[:id])
-  end
 
   def author_params
     params.require(:author).permit(:name, :cpf)
